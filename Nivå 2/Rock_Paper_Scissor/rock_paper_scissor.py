0526c2a216df3e5077f2667de51b674472b74a03
Cwack <t92@gmx.com>
2024-03-04 10:10:15 +0100

    A very basic rock paper scissor game with images

--- Nivå 2/Rock_Paper_Scissor/rock_paper_scissor.py
+++ Nivå 2/Rock_Paper_Scissor/rock_paper_scissor.py
@@ -1,25 +1,43 @@
 #Rock Paper Scissors
 import pygame as game
 import sys
+import random
+
+def addImage(display, image_location, height, width):
+    img1 = game.image.load(image_location)
+    display.blit(game.transform.scale(img1, (height, width)), (200, 100))
+    game.display.flip()
+    #game.time.wait(5000)
+
+def ImageRoulette():
+    img_arr = ["rock.jpg", "scissor.jpg", "paper.jpg"]
+    img_loc = "C:\\Users\\t92\\Documents\\Vscode\\python-skole\\Nivå 2\\Rock_Paper_Scissor\\"
+    for i in range(3):
+        addImage(display, img_loc + img_arr[i], 400, 400)
+        game.time.wait(200)
+
+def run():
+    running = True
+
+    while running: 
+        for event in game.event.get():
+            if event.type == game.QUIT:
+                running = False
+
+        game.display.flip()
+
 
 game.init()
-
 display = game.display.set_mode((800, 600))
-
-img1 = game.image.load("C:\\Users\\t92\\Documents\\Vscode\\python-skole\\Nivå 2\\Rock_Paper_Scissor\\rock2.jpg")
-
-display.blit(game.transform.scale(img1, (400, 400)), (10, 10))
-
-game.display.flip()
-game.time.wait(5000)
-
 game.display.set_caption("Stein, Saks, Papir!")
 
-running = True
 
-while running: 
-    for event in game.event.get():
-        if event.type == game.QUIT:
-            running = False
+for i in range(5):
+    ImageRoulette() 
 
-    game.display.flip()
+#Create the random image
+img_arr = ["rock.jpg", "scissor.jpg", "paper.jpg"]
+img_loc = "C:\\Users\\t92\\Documents\\Vscode\\python-skole\\Nivå 2\\Rock_Paper_Scissor\\"
+addImage(display, img_loc + img_arr[random.randint(0,3)], 400, 400)
+#game.display.flip()
+run()
